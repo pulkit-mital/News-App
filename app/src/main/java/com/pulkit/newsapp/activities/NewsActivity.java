422d206ddb9a8391158076fcf83723e965f116df
pulkit-mital <pulkit.mital@gmail.com>
2019-03-15 19:05:06 +0530

    <feat>(core): Add Databing, connect view model with repository
    Add databinding to show the data and get data from repository inside viewmodel using rxjava which will update view using live data

--- app/src/main/java/com/pulkit/newsapp/activities/NewsActivity.java
+++ app/src/main/java/com/pulkit/newsapp/activities/NewsActivity.java
@@ -2,16 +2,103 @@ package com.pulkit.newsapp.activities;
 
 import android.os.Bundle;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.pulkit.newsapp.R;
+import com.pulkit.newsapp.databinding.ActivityNewsBinding;
+import com.pulkit.newsapp.model.NewsArticle;
+import com.pulkit.newsapp.utils.NetworkUtil;
+import com.pulkit.newsapp.viewmodel.NewsViewModel;
+import com.pulkit.newsapp.viewmodel.NewsViewModelFactory;
+
+import java.util.List;
+
+import javax.inject.Inject;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import dagger.android.AndroidInjection;
 
 public class NewsActivity extends AppCompatActivity {
 
+    ActivityNewsBinding activityNewsBinding;
+
+    @Inject
+    NewsViewModelFactory newsViewModelFactory;
+
+    NewsViewModel newsViewModel;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news);
+        activityNewsBinding = DataBindingUtil.setContentView(this, R.layout.activity_news);
+        initializeToolbar();
+        initializeViewModel();
+        initializeView();
+
+
+    }
+
+    private void initializeToolbar() {
+        activityNewsBinding.layoutToolBar.toolbar.setTitle(getResources().getString(R.string.app_name));
+    }
+
+    private void initializeViewModel() {
+        newsViewModel = ViewModelProviders.of(this, newsViewModelFactory).get(NewsViewModel.class);
+    }
+
+    private void initializeView() {
+        activityNewsBinding.setNewsViewModel(newsViewModel);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initializeObservers();
+    }
+
+    private void initializeObservers() {
+        newsViewModel.getNewsArticles().observe(this, new Observer<List<NewsArticle>>() {
+            @Override
+            public void onChanged(List<NewsArticle> newsArticles) {
+            }
+        });
+
+        newsViewModel.getArticles().observe(this, new Observer<List<NewsArticle>>() {
+            @Override
+            public void onChanged(List<NewsArticle> newsArticles) {
+            }
+        });
+
+        newsViewModel.getErrorMessage().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                Snackbar.make(activityNewsBinding.getRoot(), s, Snackbar.LENGTH_SHORT).show();
+                if (NetworkUtil.isNetworkConnected(getApplicationContext())) {
+                    newsViewModel.getArticles();
+                }
+            }
+        });
+
+        newsViewModel.getIsProgressVisible().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer aBoolean) {
+                activityNewsBinding.progressBar.setVisibility(aBoolean);
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (newsViewModel != null) {
+            newsViewModel.getNewsArticles().removeObservers(this);
+            newsViewModel.getArticles().removeObservers(this);
+            newsViewModel.getErrorMessage().removeObservers(this);
+            newsViewModel.getIsProgressVisible().removeObservers(this);
+        }
     }
 }
--- app/src/main/java/com/pulkit/newsapp/viewmodel/NewsViewModel.java
+++ app/src/main/java/com/pulkit/newsapp/viewmodel/NewsViewModel.java
@@ -1,10 +1,21 @@
 package com.pulkit.newsapp.viewmodel;
 
+import android.view.View;
+
+import com.pulkit.newsapp.model.NewsArticle;
 import com.pulkit.newsapp.respository.NewsRepository;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * The class help us to fetch data using repository and will update UI
@@ -13,9 +24,64 @@ import androidx.lifecycle.ViewModel;
 public class NewsViewModel extends ViewModel {
 
     private NewsRepository newsRepository;
+    private Subscription newsSubscription;
+    private MutableLiveData<List<NewsArticle>> newsArticles = new MutableLiveData<>();
+    private MutableLiveData<String> errorMessage = new MutableLiveData<>();
+    private MutableLiveData<Integer> isProgressVisible = new MutableLiveData<>();
 
     @Inject
     public NewsViewModel(NewsRepository newsRepository) {
         this.newsRepository = newsRepository;
     }
+
+    public void getNews() {
+
+        isProgressVisible.setValue(View.VISIBLE);
+        newsSubscription = newsRepository.getNews()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<NewsArticle>>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        errorMessage.setValue(e.getMessage());
+                        isProgressVisible.setValue(View.GONE);
+                    }
+
+                    @Override
+                    public void onNext(List<NewsArticle> news) {
+                        newsArticles.setValue(news);
+                        isProgressVisible.setValue(View.GONE);
+                    }
+                });
+    }
+
+    public LiveData<List<NewsArticle>> getArticles() {
+        return newsRepository.loadArticles();
+    }
+
+    public MutableLiveData<List<NewsArticle>> getNewsArticles() {
+        return newsArticles;
+    }
+
+    public MutableLiveData<String> getErrorMessage() {
+        return errorMessage;
+    }
+
+    public MutableLiveData<Integer> getIsProgressVisible() {
+        return isProgressVisible;
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        if (newsSubscription != null && !newsSubscription.isUnsubscribed()) {
+            newsSubscription.unsubscribe();
+        }
+    }
 }
+
