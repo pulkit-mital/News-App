b83c02621ed5e7cde54ab764d19e763b6873c38d
pulkit-mital <pulkit.mital@gmail.com>
2019-03-15 17:08:57 +0530

    <fix>(core): Extend RoomDatabase
    Extends RoomDatabase class to NewsDatabase for database creation
    
    <footer>

--- app/src/main/java/com/pulkit/newsapp/database/NewsDatabase.java
+++ app/src/main/java/com/pulkit/newsapp/database/NewsDatabase.java
@@ -3,12 +3,13 @@ package com.pulkit.newsapp.database;
 import com.pulkit.newsapp.model.NewsArticle;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 /**
  * Room Database to store various entities or tables in the database
  * has instance of dao to operate CRUD instruction
  */
 @Database(entities = {NewsArticle.class}, version = 1)
-public abstract class NewsDatabase {
+public abstract class NewsDatabase extends RoomDatabase {
     public abstract NewsArticleDao newsArticleDao();
 }
